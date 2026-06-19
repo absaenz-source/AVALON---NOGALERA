973348123b0af3a4dc7b0680b6a70e08fe4cdb25
absaenz-source <ab.saenz@hotmail.com>
2026-06-19 11:13:02 -0600

    Remove server update alert from app.py

--- app.py
+++ app.py
@@ -158,10 +158,8 @@ with st.sidebar:
 # 5. ÁREA CENTRAL DINÁMICA
 # ==============================================================================
 
-st.error("🚨 ¡ATENCIÓN SITEMA! SI ESTÁS LEYENDO ESTO, EL SERVIDOR SÍ ESTÁ ACTUALIZADO 🚨")
 if st.session_state.opcion_menu == "Dashboard":
     mostrar_dashboard()
-
 
 elif st.session_state.opcion_menu == "Finanzas":
     mostrar_finanzas()
